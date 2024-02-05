0d0c354af500e76f050badaab6c8880613f3b392
Shinra <93828376+S-h-i-n-r-a@users.noreply.github.com>
2024-02-06 00:13:18 +0100

    Created main mod file and patch for max stack size text removal.

---
+++ Patches/InventoryGrid_UpdateGui.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HarmonyLib;
+using TMPro;
+
+
+namespace MaxStackTextRemover.Patches
+{
+    [HarmonyPatch(typeof(InventoryGrid), nameof(InventoryGrid.UpdateGui))]
+    public static class InventoryGrid_UpdateGui
+    {
+        public static void Finalizer(InventoryGrid __instance)
+        {
+            var width = __instance.m_inventory.GetWidth();
+            var inventoryItems = __instance.m_inventory.GetAllItems();
+
+            foreach (ItemDrop.ItemData allItem in inventoryItems)
+            {
+                if (allItem.m_shared.m_maxStackSize > 1)
+                {
+                    var element = __instance.GetElement(allItem.m_gridPos.x, allItem.m_gridPos.y, width);
+                    element.m_amount.text = allItem.m_stack.ToString();
+                    element.m_amount.alignment = TextAlignmentOptions.Right;
+                }
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+
+namespace MaxStackTextRemover
+{
+    [BepInPlugin(Guid, Name, Vers)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string Guid = "dev.shinra.nostackmaxdisp";
+        public const string Name = "Max Stack Text Remover";
+        public const string Vers = "2024.205.2043";
+
+        public static ManualLogSource UnityLogger;
+
+
+        private void Awake()
+        {
+            UnityLogger = Logger;
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            UnityLogger.LogMessage($"Running \"{Name}\" v{Vers}, thanks for using my mod! -Shinra.");
+        }
+    }
+}
